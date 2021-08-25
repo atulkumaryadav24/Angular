136fb8a1569e7657379664ca5493702d1c53c18a
Atul <atul.blast@gmail.com>
2021-08-26 02:49:44 +0530

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+(function(){
+    'use strict';
+    angular.module('AngularApp',[])
+    .controller('AngularController',function ($scope){
+        $scope.guilt= function emi(){ return "Emienm";
+        }
+    });
+})();
