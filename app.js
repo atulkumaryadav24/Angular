4b3edc61bd4ba7390954bce33109448c82de2023
Atul <atul.blast@gmail.com>
2021-08-27 19:10:52 +0530

    CapLetters

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 (function(){
     'use strict';
     angular.module('AngularApp',[])
-    .controller('AngularController',function ($scope){
-        $scope.guilt= function emi(){ return "Emienm";
-        }
-    });
-})();
+    .controller('AngularController',Fun);
+    Fun.$injector=['$scope','$filter']
+    function Fun($scope, $filter) {
+        $scope.name = "";
+        // $scope.num = 0;
+        // $scope.disval = function (){
+        //     var sum= CharCount($scope.name);
+        //     $scope.num=sum;
+        $scope.caps = function () {
+            var cap = $filter('uppercase');
+            $scope.name = cap($scope.name);
+        };
+    }
+        // function CharCount(string){
+        //     var count=0;
+        //     for(var i=0;i<string.length;i++){
+        //         count+=string.charCodeAt(i);
+        //     }
+        //     return count;
+        // }
+    })();
