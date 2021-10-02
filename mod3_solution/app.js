9c9f5185f042902199489d30d8acf122f1bef5a5
Atul <atul.blast@gmail.com>
2021-10-03 01:00:01 +0530

    New Folder

--- mod3_solution/app.js
+++ mod3_solution/app.js
@@ -2,49 +2,65 @@
 'use strict';
 angular.module('NarrowItDownApp',[])
 .controller('NarrowItDownController',NarrowItDownController)
-.service('MenuSearch',MenuSearchService)
-.directive('foundItem',foundItemDirective)
-.constant('siteUrl','https://davids-restaurant.herokuapp.com/menu_items.json');
+.controller('FoundItemDirectiveController',FoundItemDirectiveController)
+.service('MenuSearchService',MenuSearchService)
+.directive('foundItems',foundItemDirective);
 NarrowItDownController.$inject['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
+function NarrowItDownController(MenuSearchService){
     var narrow = this;
     narrow.searchTerm="";
-    narrow.found = MenuSearchService.getMatchedMenuItems(searchTerm);
+    narrow.found=[];
+    narrow.search = function (){
+        MenuSearchService.getMatchedMenuItems(narrow.searchTerm).then(function (result) {
+        narrow.found = result;
+        });
+    };
     narrow.onRemove = MenuSearchService.onRemove;
 }
 function foundItemDirective(){
     var ddo={
     scope:{
-    foundItems: '<'
-    onRemove: '&'}
+    found: '<',
+    onRemove: '&'
+    },
+    controller:'FoundItemDirectiveController as narrowdir',
+    bindToController : true,
     templateUrl:'restaurant.html'
 };
 return ddo;
 }
-MenuSearchService.$inject['siteUrl','$http']
-function MenuSearchService(siteUrl,$http){
+function FoundItemDirectiveController(){
+    var narrowdir = this;
+    narrowdir.ifEmpty=function (){
+        return narrowdir.found.lenght==0;
+    };
+}
+MenuSearchService.$inject['$http']
+function MenuSearchService($http){
     var service = this;
     service.getMatchedMenuItems=function (searchTerm){
         return $http({
             method:'GET',
-            url:siteUrl
+            url:"https://davids-restaurant.herokuapp.com/menu_items.json"
         }).then(
-            function (result){
+            function success(result){
                 var resultItems=result.data.menu_items;
-                var foundItems = [];
-                for(var i=0;i<resultItems.lenght;i++){
+                var found = [];
+                for(var i=0;i<resultItems.length;i++){
                     if(searchTerm!=''&resultItems[i].description.indexOf(searchTerm!=-1)){
-                        foundItems.append({
+                        found.append({
                             name:resultItems[i].name,
                             description:resultItems[i].description
                         });
                     }
                 }
-                return foundItems;
-            });
+                return found;
+            }, function error(error) {
+
+      });
     };
     service.onRemove = function (itemIndex) {
-    items.splice(itemIndex, 1);
-  };
+    item.splice(itemIndex, 1);
+};
 }
 })();
